9275d72248eb654d73814e7d0d1a6934f3acf684
wilsonetoz <82057984+wilsonetoz@users.noreply.github.com>
2025-06-30 21:04:39 -0300

    Update DF.py

--- DF.py
+++ DF.py
@@ -5,14 +5,11 @@ from cryptography.hazmat.primitives import serialization # Certifique-se desta i
 # Gerar os parâmetros DH
 parameters = dh.generate_parameters(generator=2, key_size=1024, backend=default_backend())
 
-# Serializar os parâmetros para que possam ser usados em ambos os scripts
-# CORREÇÃO AQUI: Usamos parameter_bytes diretamente no objeto parameters
 pem_parameters = parameters.parameter_bytes(
     encoding=serialization.Encoding.PEM,
-    format=serialization.ParameterFormat.PKCS3 # Formato padrão para parâmetros DH
+    format=serialization.ParameterFormat.PKCS3
 )
 
-# Salvar em um arquivo (opcional, mas boa prática)
 with open("dh_params.pem", "wb") as f:
     f.write(pem_parameters)
 
